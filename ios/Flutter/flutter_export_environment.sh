3ee937a6b68edb87043981d68b78253b85b9c0b9
Vladlen <wertizator@gmail.com>
2025-10-02 17:01:35 +0300

    hh

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Work\all_Sdk\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Work\project\AirAlarm-flutter"
+export "FLUTTER_ROOT=/Users/mac/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mac/Projects/AirAlarm-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.6.1"
 export "FLUTTER_BUILD_NUMBER=1"
